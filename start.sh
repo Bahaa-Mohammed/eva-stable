1c4e684320d79bb781fba48153a637a0d4a76abe
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-20 20:24:00 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/drmhdh/subinpsmediasearchbot
+  git clone https://github.com/drmhdh/DENTTECHLIBRARY2022
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /subinpsmediasearchbot
+  git clone $UPSTREAM_REPO /DENTTECHLIBRARY2022
 fi
-cd /subinpsmediasearchbot
+cd /DENTTECHLIBRARY2022
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
