b7fe47f9e5fa677cb8ab4fa47c6a32968afd45b9
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-24 22:14:26 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -440,8 +440,8 @@ async def cb_handler(client: Client, query: CallbackQuery):
                 ],[
                 InlineKeyboardButton("➕Join 🦷Discussion Group➕", url="https://t.me/dent_tech_for_u")
                 ],[
-                InlineKeyboardButton("📺 𝔻𝕖𝕞𝕠𝕟𝕤𝕥𝕣𝕒𝕥𝕚𝕠𝕟 𝕍𝕚𝕕𝕖𝕠 🧭", url="https://t.me/grand_dental_library/378?comment=75870")
-                               
+                InlineKeyboardButton("📺 𝔻𝕖𝕞𝕠 🧭", url="https://t.me/grand_dental_library/378?comment=75870"),
+                InlineKeyboardButton("🤔 𝔽𝔸ℚ𝕤 💡", url="https://t.me/dental_backup/241")         
                 ],[            
                 InlineKeyboardButton("🎁 Donate & Support 🎁", url="https://t.me/dental_backup/180")
             ]]   
