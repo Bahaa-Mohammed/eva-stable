6102a2d96dee4c1f902b03877cf6dc9a0bd0d568
Dr.Strange <71588447+drmhdh@users.noreply.github.com>
2022-01-25 17:59:39 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -81,7 +81,7 @@ async def give_filter(client,message):
         await auto_filter(client, message)   
         await hashrequests(client, message)
            
-@Client.on_message(filters.text & filters.private & filters.incoming & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming)
+@Client.on_message(filters.text & filters.private & filters.incoming & ~filters.edited & filters.user(AUTH_USERS) if AUTH_USERS else filters.text & filters.private & filters.incoming & ~filters.edited)
 async def filter(client, message):  
     if message.text.startswith("/"):
         return        
